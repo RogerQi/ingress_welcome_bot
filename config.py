1bab104648c1a848d4daba69f7b0c8430018b727
Tw0fourths <twofs0.5@gmail.com>
2016-06-18 13:47:02 +0800

    Create config.py
    placeholder for private parameters.

---
+++ config.py
@@ -0,0 +1,15 @@
+#-*-coding: UTF-8-*-
+login = 'your_account@gmail.com'
+pwd = 'your_password'
+anli = "What you wanna send in faction comm to new comers"
+user_agent = 'Your_user_agent'
+config.bot_latitude =  # latitude where the messages will be sent.
+config.bot_longitude = # longitude where the messages will be sent.
+config.minlat1 = #lower latitude of first area the bot will scan
+config.minlng1 = #left longitude of first are the bot will scan
+config.maxlat1 = #upper latitude
+config.maxlng1 = #right longitude
+config.minlat2 = #paras for second area
+config.minlng2 =
+config.maxlat2 =
+config.maxlng2 =
